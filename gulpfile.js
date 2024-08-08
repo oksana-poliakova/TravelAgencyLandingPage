0c5c95ca547911e5b1c584a555800b014ece5eee
Oksana Poliakova <it.oksana.poliakova@gmail.com>
2024-08-08 14:03:02 +0200

    Added the services section

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,29 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
-const uglify = require('gulp-uglify'); // Добавьте это
+const uglify = require('gulp-uglify'); 
 
-// Путь к SCSS и CSS файлам
 const paths = {
     scss: 'src/scss/**/*.scss',
     css: 'public/css/'
 };
 
-// Задача для компиляции SCSS в CSS
 gulp.task('sass', function() {
     return gulp.src(paths.scss)
         .pipe(sass().on('error', sass.logError))
         .pipe(cleanCSS())
-        .pipe(gulp.dest(paths.css)); // Убедитесь, что выходной путь указывает на public/css
+        .pipe(gulp.dest(paths.css)); 
 });
 
-// Задача для минификации JavaScript (если используется)
 gulp.task('uglify', function() {
-    return gulp.src('src/js/**/*.js') // Убедитесь, что путь правильный
+    return gulp.src('src/js/**/*.js') 
         .pipe(uglify())
-        .pipe(gulp.dest('public/js')); // Убедитесь, что выходной путь правильный
+        .pipe(gulp.dest('public/js')); 
 });
 
-// Задача для наблюдения за изменениями в SCSS и JS файлах
 gulp.task('watch', function() {
     gulp.watch(paths.scss, gulp.series('sass'));
-    gulp.watch('src/js/**/*.js', gulp.series('uglify')); // Добавьте это
+    gulp.watch('src/js/**/*.js', gulp.series('uglify')); 
 });
 
-// Задача по умолчанию
-gulp.task('default', gulp.series('sass', 'uglify', 'watch')); // Обновите это
+gulp.task('default', gulp.series('sass', 'uglify', 'watch')); 
